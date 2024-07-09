fb4b412a1a71e23169e555f038e0f9834c8597cb
Ansak Mahir <ansarmmm.21@uom.lk>
2024-07-09 17:02:21 +0530

    components and playing around css and import export

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import Header from './Header';
+import MainContent from './MainComponent';
+import Footer from './Footer';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
+
+
+
+function Staticpage(){
+  return(
+    <>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </>
+  )
+}
+
+const id=document.getElementById("root")
+const route = ReactDOM.createRoot(id)
+route.render(
+  <Staticpage/>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
